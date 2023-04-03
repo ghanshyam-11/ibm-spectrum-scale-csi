fa017628e8c1225161b75db1c41075d7ee261e47
ghanshyam-11 <ghanshyam.sharma1@ibm.com>
2023-04-03 14:25:23 +0530

    Fix for missing events
    Signed-off-by: ghanshyam-11 <ghanshyam.sharma1@ibm.com>

--- operator/main.go
+++ operator/main.go
@@ -25,11 +25,14 @@ import (
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	"go.uber.org/zap/zapcore"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"sigs.k8s.io/controller-runtime/pkg/cache"
@@ -144,10 +147,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	broadcaster := record.NewBroadcasterWithCorrelatorOptions(record.CorrelatorOptions{QPS: 1. / 30.})
+	broadcaster.StartStructuredLogging(2)
+	broadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{
+		Interface: kubernetes.NewForConfigOrDie(mgr.GetConfig()).CoreV1().Events(""),
+	})
+
 	if err = (&controllers.CSIScaleOperatorReconciler{
 		Client:   mgr.GetClient(),
 		Scheme:   mgr.GetScheme(),
-		Recorder: mgr.GetEventRecorderFor("CSIScaleOperator"),
+		Recorder: broadcaster.NewRecorder(scheme, corev1.EventSource{Component: "CSIScaleOperator"}),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "CSIScaleOperator")
 		os.Exit(1)
